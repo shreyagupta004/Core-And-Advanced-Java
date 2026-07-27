1cf22aa1b5ee21c917c62b71852b4e955c8003e6
shreya gupta <iamshreyagupta13@gmail.com>
2026-07-27 22:07:56 +0530

    add lecture 43

--- lec_42_staticVsInstanceMethod.java
+++ lec_42_staticVsInstanceMethod.java
@@ -1,9 +1,9 @@
 import java.util.*;
-public class lec_42_staticVsInstance {
+public class lec_42_staticVsInstanceMethod {
     public static void main(String args[]){
         student.print();   // static can be accessed without making an object----
         student s1 = new student();
-        s1.display();     // non-statuc need to make object to get accesed----
+        s1.display();     // non-static need to make object to get accesed----
     }
     public static class student{
         public static void print(){
---
+++ lec_43_staticVsInstanceVariable.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class lec_43_staticVsInstanceVariable {
+    public static void main(String args[]){
+        practice p =new practice("shreya");
+        p.print();
+        practice p2 = new practice("anshul");
+        p2.print();
+        p.print();
+
+
+    }
+    public  static class practice{
+        public static String name;
+        public practice (String name){
+            this.name = name;
+        }
+        public void print(){
+            System.out.println("my name is " + name);
+        }
+    }
+}
